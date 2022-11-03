ad5f6ee7ecad94c85f00c85fbc9ebf027a04c3ae
Julian <julianobandovelez@cmail.carleton.ca>
2022-11-03 14:38:37 -0400

    Included some definitions and declared functions

---
+++ Robot_Movement/Robot_Movement.ino
@@ -0,0 +1,62 @@
+#define RIGHT false
+#define LEFT true 
+
+// Line Follower Sensor Pin Out
+#Define LFS_R_pin 23
+#Define LFS_M_pin 25
+#Define LSF_L_pin 27
+
+////Stepper Motors Pin Out
+//Back Left
+#Define BL_Wheel_Enable 47
+#Define BL_Wheel_Direction 2
+//Back Right
+#Define BR_Wheel_Enable 51
+#Define BR_Wheel_Direction 4
+//Front Left
+#Define FL_Wheel_Enable 49
+#Define FL_Wheel_Direction 3
+//Front Right
+#Define FR_Wheel_Enable 53
+#Define FR_Wheel_Direction 5
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void moveStraight(){
+  //Move Straight code
+
+}
+
+void move_distance(){
+
+
+}
+
+
+void turn27(bool direction){
+  //Move Straight code
+
+}
+
+void turn45(){
+  //Move Straight code
+
+}
+
+void turn90(){
+  //Move Straight code
+
+}
+
+void turn180(){
+  //Move Straight code
+
+}
