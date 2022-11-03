28045288d0bbb78fc28756aac82eab69706d2c8e
Julian <julianobandovelez@cmail.carleton.ca>
2022-11-03 14:40:13 -0400

    Fixed typos

--- Robot_Movement/Robot_Movement.ino
+++ Robot_Movement/Robot_Movement.ino
@@ -2,23 +2,23 @@
 #define LEFT true 
 
 // Line Follower Sensor Pin Out
-#Define LFS_R_pin 23
-#Define LFS_M_pin 25
-#Define LSF_L_pin 27
+#define LFS_R_pin 23
+#define LFS_M_pin 25
+#define LSF_L_pin 27
 
 ////Stepper Motors Pin Out
 //Back Left
-#Define BL_Wheel_Enable 47
-#Define BL_Wheel_Direction 2
+#define BL_Wheel_Enable 47
+#define BL_Wheel_Direction 2
 //Back Right
-#Define BR_Wheel_Enable 51
-#Define BR_Wheel_Direction 4
+#define BR_Wheel_Enable 51
+#define BR_Wheel_Direction 4
 //Front Left
-#Define FL_Wheel_Enable 49
-#Define FL_Wheel_Direction 3
+#define FL_Wheel_Enable 49
+#define FL_Wheel_Direction 3
 //Front Right
-#Define FR_Wheel_Enable 53
-#Define FR_Wheel_Direction 5
+#define FR_Wheel_Enable 53
+#define FR_Wheel_Direction 5
 
 void setup() {
   // put your setup code here, to run once:
