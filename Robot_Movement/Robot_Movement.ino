cd38b1ab0d388d31511717cdba93f4d0d2f20b68
Julian <julianobandovelez@cmail.carleton.ca>
2022-11-03 20:54:58 -0400

    Included Movement Functions and more definitions

--- Robot_Movement/Robot_Movement.ino
+++ Robot_Movement/Robot_Movement.ino
@@ -21,8 +21,25 @@
 #define FR_Wheel_Direction 5
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LFS_R_pin, INPUT);   //LFS_R  OUT2
+  pinMode(LFS_M_pin, INPUT);   //LFS_M  OUT3
+  pinMode(LSF_L_pin, INPUT);   //LFS_L  OUT1
 
+  // BL Wheel
+  pinMode(BL_Wheel_Enable, OUTPUT); // Enable
+  pinMode(BL_Wheel_Direction, OUTPUT); // Direction
+
+  // BR Wheel
+  pinMode(BR_Wheel_Enable, OUTPUT); // Enable
+  pinMode(BR_Wheel_Direction, OUTPUT); // Direction
+
+  // FL Wheel
+  pinMode(FL_Wheel_Enable, OUTPUT); // Enable
+  pinMode(FL_Wheel_Direction, OUTPUT); // Direction
+
+  // FR Wheel
+  pinMode(FR_Wheel_Enable, OUTPUT); // Enable
+  pinMode(FR_Wheel_Direction, OUTPUT); // Direction
 }
 
 void loop() {
@@ -31,8 +48,17 @@ void loop() {
 }
 
 void moveStraight(){
-  //Move Straight code
+  // Directions all Forward
+  digitalWrite(BL_Wheel_Direction, LOW);
+  digitalWrite(FL_Wheel_Direction, LOW);
+  digitalWrite(BR_Wheel_Direction, HIGH);
+  digitalWrite(FR_Wheel_Direction, HIGH);
 
+  //Enable 
+  digitalWrite(BL_Wheel_Enable, HIGH);
+  digitalWrite(FL_Wheel_Enable, HIGH);
+  digitalWrite(BR_Wheel_Enable, HIGH);
+  digitalWrite(FR_Wheel_Enable, HIGH);
 }
 
 void move_distance(){
@@ -40,6 +66,14 @@ void move_distance(){
 
 }
 
+void stop(){
+  //Disable 
+  digitalWrite(BL_Wheel_Enable, LOW);
+  digitalWrite(FL_Wheel_Enable, LOW);
+  digitalWrite(BR_Wheel_Enable, LOW);
+  digitalWrite(FR_Wheel_Enable, LOW);
+
+}
 
 void turn27(bool direction){
   //Move Straight code
