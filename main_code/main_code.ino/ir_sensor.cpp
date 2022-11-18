fff13969c348ab742d0b9a9d43621066961549e1
julian-carleton <71390371+julian-carleton@users.noreply.github.com>
2022-11-18 00:02:11 -0500

    Added sensor check for rotating motion

--- main_code/main_code.ino/ir_sensor.cpp
+++ main_code/main_code.ino/ir_sensor.cpp
@@ -1,1 +1,29 @@
 #include "ir_sensor.h"
+
+bool turn_check_front_right(int FR_Pin){
+  int counter = 0;
+  for (int i=0; i < 3; i++){
+    bool temp_detect = digitalRead(FR_Pin);
+    if (temp_detect){
+      counter += 1;
+    }
+    if (counter == 3){
+      return true;
+    }
+  }
+  return false;
+}
+
+bool turn_check_back_left(int BL_Pin){
+  int counter = 0;
+  for (int i=0; i < 3; i++){
+    bool temp_detect = digitalRead(BL_Pin);
+    if (temp_detect){
+      counter += 1;
+    }
+    if (counter == 3){
+      return true;
+    }
+  }
+  return false;
+}
--- main_code/main_code.ino/ir_sensor.h
+++ main_code/main_code.ino/ir_sensor.h
@@ -2,6 +2,7 @@
 #define IR_SENSOR_H
 #include <Arduino.h>
 
-
+bool turn_check_front_right(int FR_Pin);
+bool turn_check_back_left(int BL_Pin);
 
 #endif
