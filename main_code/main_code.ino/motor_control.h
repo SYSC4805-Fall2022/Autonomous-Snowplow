11da2ae221284729a8adf2caa962dd945c5bb5c2
alinaahmad1 <97753944+alinaahmad1@users.noreply.github.com>
2022-11-17 23:57:55 -0500

    Update motor_control.h
    added functions used in motor_control.cpp

--- main_code/main_code.ino/motor_control.h
+++ main_code/main_code.ino/motor_control.h
@@ -2,6 +2,13 @@
 #define MOTOR_CONTROL_H
 #include <Arduino.h>
 
-
+void forward(int BL_Direction, int BR_Direction, int FL_Direction, int FR_Direction);
+void backward(int BL_Direction, int BR_Direction, int FL_Direction, int FR_Direction);
+void left(int BL_Direction, int BR_Direction, int FL_Direction, int FR_Direction);
+void right(int BL_Direction, int BR_Direction, int FL_Direction, int FR_Direction);
+void enable_on(int BL_Enable, int BR_Enable, int FL_Enable, int FR_Enable);
+void enable_off(int BL_Enable, int BR_Enable, int FL_Enable, int FR_Enable);
+void enable_left_turn(int BL_Enable, int BR_Enable, int FL_Enable, int FR_Enable);
+void enable_right_turn(int BL_Enable, int BR_Enable, int FL_Enable, int FR_Enable);
 
 #endif
