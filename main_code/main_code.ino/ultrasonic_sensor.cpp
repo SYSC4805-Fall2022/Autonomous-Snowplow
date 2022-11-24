a2b2cb0963aa00a8253feed88f4d5cde845f3a29
julian-carleton <71390371+julian-carleton@users.noreply.github.com>
2022-11-24 15:14:26 -0500

    fixed ultrasonic debounce

--- main_code/main_code.ino/ultrasonic_sensor.cpp
+++ main_code/main_code.ino/ultrasonic_sensor.cpp
@@ -1,7 +1,11 @@
 #include "ultrasonic_sensor.h"
 
 int get_distance_cm(EZDist object){
-  return object.cm();
+  int dist = object.cm();
+  if (dist > 3){
+    return dist;
+  }
+  return 100;
 }
 
 int get_average_distance_cm(EZDist object, int num_iterations){
@@ -13,7 +17,9 @@ int get_average_distance_cm(EZDist object, int num_iterations){
 }
 
 bool object_detection_ultrasonic(EZDist object, int threshold_distance_cm){
-  if (get_distance_cm(object) < threshold_distance_cm){
+  int dist = get_distance_cm(object);
+  if (dist < threshold_distance_cm){
+    Serial.println(dist);
     return true;
   }
   return false;
