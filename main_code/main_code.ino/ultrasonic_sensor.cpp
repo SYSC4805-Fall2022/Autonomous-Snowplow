6bca86a729a68cd43ab8cb60f51d85928e0345f4
alinaahmad1 <97753944+alinaahmad1@users.noreply.github.com>
2022-11-26 15:37:55 -0500

    Update ultrasonic_sensor.cpp
    added documentation

--- main_code/main_code.ino/ultrasonic_sensor.cpp
+++ main_code/main_code.ino/ultrasonic_sensor.cpp
@@ -1,8 +1,13 @@
+# The Ultrasonic sensor is used to measures the distance between the the robot and a potential obstacle
+# The EZDist object zip file is used to get the distance in centimeters and uses this distance to find the average distance
+
+
 #include "ultrasonic_sensor.h"
 
 int get_distance_cm(EZDist object){
   return object.cm();
 }
+
 
 int get_average_distance_cm(EZDist object, int num_iterations){
   int total = 0;
@@ -11,6 +16,9 @@ int get_average_distance_cm(EZDist object, int num_iterations){
   }
   return total / num_iterations;
 }
+
+//Returns True if object distance is less than the threhold distance
+//Detects an obstacle while the robot is moving within the taped-area
 
 bool object_detection_ultrasonic(EZDist object, int threshold_distance_cm){
   if (get_distance_cm(object) < threshold_distance_cm){
